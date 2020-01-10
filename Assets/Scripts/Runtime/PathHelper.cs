ce7427c474a649804efb21b651a69198aa147f17
Lin <li18202213260@gmail.com>
2020-01-11 00:35:44 +0800

    first commit. Relocate folder and  create some assembly

---
+++ Assets/Scripts/Runtime/PathHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PathHelper
+{
+    public static string GetHotDll { get; } = "Hotfix";
+    public static string GetRuntimeDll { get; } = "Runtime";
+    public static string GetHotNamespace { get; } = "Hotfix.";
+    public static string GetHotDllPath { get; } = "Library/ScriptAssemblies/Hotfix.dll";
+    public static string GetRuntimeDllPath { get; } = "./Library/ScriptAssemblies/Runtime.dll";
+    public static string GetAutoDictPath { get; } = "Assets/Scripts/Runtime/Auto";
+    public static string GetAutoBuildDelegatePath { get; } = $"{GetAutoDictPath}/ILRegType_{0}.cs";
+    public static string GetAutoBuildObjectsPath { get; } =  $"{GetAutoDictPath}/GenObjects_{0}.cs";
+    public static string GetAutoBuildDelegateBridgePath { get; } = $"{GetAutoDictPath}/GenDelegateBridge_{0}.cs";
+}
--- Assets/XIL/ILSource/ILRuntime/Other/ByReferenceKeyComparer.cs
+++
@@ -1,19 +0,0 @@
-#if USE_HOTusing System.Collections.Generic;
-using System.Runtime.CompilerServices;
-
-namespace ILRuntime.Other
-{
-    class ByReferenceKeyComparer<T> : IEqualityComparer<T>
-    {
-        public bool Equals(T x, T y)
-        {
-            return object.ReferenceEquals(x, y);
-        }
-
-        public int GetHashCode(T obj)
-        {
-            return RuntimeHelpers.GetHashCode(obj);
-        }
-    }
-}
-#endif
